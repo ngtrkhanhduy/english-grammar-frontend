71dad4c308e7b63673717501fb41e32bdf0a0c16
Dennis Nguyen <ngtrkhanhduy1308@gmail.com>
2024-12-01 10:23:38 +0700

    format code with prettierrc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-      <h1>Hello World!!!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+            <h1>Hello World!!!</h1>
+        </div>
+    );
 }
 
 export default App;
