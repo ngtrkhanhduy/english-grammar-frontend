942faf0c38b1183d93fc94e30e0efed1d2fd27d5
Dennis Nguyen <ngtrkhanhduy1308@gmail.com>
2024-12-21 10:52:07 +0700

    update structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { privateRoutes, publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+
 function App() {
     return (
-        <div className="App">
-            <header className="App-header"></header>
-            <h1>Hello World!!!</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+
+                    {privateRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        return <Route key={index} path={route.path} element={<Page />} />;
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+// Pages
+import Home from '~/pages/Home';
+import Profile from '~/pages/Profile';
+import Login from '~/pages/Login';
+
+// Public routes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/profile', component: Profile },
+];
+
+const privateRoutes = [{ path: '/Login', component: Login }];
+
+export { publicRoutes, privateRoutes };
